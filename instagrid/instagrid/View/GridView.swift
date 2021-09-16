edf8d53c9fe724edf42cd71334921464f344481a
Nassir <nassir.bour@gmail.com>
2021-09-16 20:32:11 +0200

    adding did tap on the plus button for adding picture with the UiImage picker controller not functional yet

--- instagrid/instagrid/View/GridView.swift
+++ instagrid/instagrid/View/GridView.swift
@@ -60,5 +60,24 @@ class GridView: UIView {
                 bottomRightSquare.isHidden = false
         }
     }
+    
+//    func didTapPlus() {
+//        let vc = UIImagePickerController()
+//        vc.sourceType = .photoLibrary
+//        vc.delegate = self
+//        vc.allowsEditing = true
+//        present(vc, animated: true)
+//    }
 }
 
+//extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+//    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+//        if let image = info[UIImagePickerController.InfoKey(rawValue: "UIImagePickerControllerEditedImage")] as? UIImage {
+//            imageView.image = image
+//        }
+//        picker.dismiss(animated: true, completion: nil)
+//    }
+//    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+//        picker.dismiss(animated: true, completion: nil)
+//    }
+//}
